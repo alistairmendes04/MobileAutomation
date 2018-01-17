21facd521f9811fc970e62fec1001c0299a82c67
jahidul2543 <jahidul2543@gmail.com>
2018-01-17 14:37:43 -0500

    Updates to GoogleMap Module

--- GoogleMap/src/main/java/home/HomePage.java
+++ GoogleMap/src/main/java/home/HomePage.java
@@ -7,22 +7,82 @@ import org.openqa.selenium.support.FindBy;
 public class HomePage extends MobileAPI {
     @FindBy(className = "android.widget.ImageView")
     public WebElement burgerBtn;
-
+    @FindBy(linkText = "Set home address")
+    public WebElement setHomeAddress;
     String menuArray[] = {"Your places", "Your timeline", "Your contributions", "Location sharing",
-            "Start driving", "Explore", "Wi-Fi only", "Tips and tricks", "Add a missing place", "Settings",
+            "Start driving", "Explore", "Tips and tricks", "Add a missing place", "Settings",
             "Help", "Send feedback", "Terms of Service"};
 
     public void getMenu() {
         burgerBtn.click();
     }
 
-    public void getYourPlaces(){
+    public void getYourPlaces() {
         getMenu();
         scrollAndClickByName(menuArray[0]);
     }
 
-    public void geYourTimeline(){
+    public void getYourTimeline() {
         getMenu();
         scrollAndClickByName(menuArray[1]);
     }
+
+    public void getYourContributions() {
+        getMenu();
+        scrollAndClickByName(menuArray[2]);
+    }
+
+    public void getLocationSharing() {
+        getMenu();
+        scrollAndClickByName(menuArray[3]);
+    }
+
+    public void getStartDriving() {
+        getMenu();
+        scrollAndClickByName(menuArray[4]);
+    }
+    public void getExplore(){
+        getMenu();
+        scrollAndClickByName(menuArray[5]);
+    }
+
+    public void getTipsAndTricks() {
+        getMenu();
+        scrollAndClickByName(menuArray[6]);
+    }
+
+    public void getAddAMissingPlace() {
+        getMenu();
+        scrollAndClickByName(menuArray[7]);
+    }
+
+    public void getSettings() {
+        getMenu();
+        scrollAndClickByName(menuArray[8]);
+    }
+
+    public void getHelp() {
+        getMenu();
+        scrollAndClickByName(menuArray[9]);
+    }
+
+    public void getSendFeedback() {
+        getMenu();
+        scrollAndClickByName(menuArray[10]);
+    }
+    public void getTermsOfServices(){
+        getMenu();
+        scrollAndClickByName(menuArray[11]);
+    }
+    public void setHomeAddressInYourPlaces(){
+        /*getMenu();
+        getYourPlaces();*/
+        burgerBtn.click();
+        getYourPlaces();
+        setHomeAddress.click();
+
+
+    }
+
+
 }
--- GoogleMap/src/test/java/testCases/testHomePage.java
+++ GoogleMap/src/test/java/testCases/testHomePage.java
@@ -9,23 +9,37 @@ import javax.lang.model.element.PackageElement;
 
 public class testHomePage extends HomePage {
     HomePage objHomePage;
-
     @BeforeMethod
     public void initializationOfObjects() {
         objHomePage = PageFactory.initElements(ad, HomePage.class);
     }
+    @Test
+    public void testMenu() {objHomePage.getMenu();}
+    @Test
+    public void testYourPlaces() {objHomePage.getYourPlaces();}
+    @Test
+    public void testYourTimeline(){ objHomePage.getYourTimeline();    }
+    @Test
+    public void testYourCOntribution(){objHomePage.getYourContributions();}
+    @Test
+    public void testLocationSharing(){objHomePage.getLocationSharing();}
+    @Test
+    public void testStartDriving(){objHomePage.getStartDriving();}
+    @Test
+    public void testExplore(){objHomePage.getExplore();}
+    @Test
+    public void testTipsAndTricks(){objHomePage.getTipsAndTricks();}
+    @Test
+    public void testAddAMissingPlace(){objHomePage.getAddAMissingPlace();}
+    @Test
+    public void testSettings(){objHomePage.getSettings();}
+    @Test
+    public void testHelp(){objHomePage.getHelp();}
+    @Test
+    public void testSendFeedBack(){objHomePage.getSendFeedback();}
+    @Test
+    public void testTermsOfService(){objHomePage.getTermsOfServices();}
+    @Test
+    public void testSetHomeAddressInYourPlace(){objHomePage.setHomeAddressInYourPlaces();}
 
-    @Test
-    public void testMenu() {
-        objHomePage.getMenu();
-    }
-
-    @Test
-    public void testYourPlaces() {
-        objHomePage.getYourPlaces();
-    }
-    @Test
-    public void testYourTimeline(){
-        objHomePage.geYourTimeline();
-    }
 }
